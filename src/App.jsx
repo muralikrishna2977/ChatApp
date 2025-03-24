95420c6ec43eb7c9da75312ae262ba185d49bfb7
muralikrishna2977 <muralikrishna1502887@gmail.com>
2025-03-25 00:54:05 +0530

    Fixed routing issue for GitHub Pages using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp.jsx";
 import SignIn from "./components/SignIn.jsx";
 import Chat from "./components/Chat.jsx";
